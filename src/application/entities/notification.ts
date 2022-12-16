0b1bb2f229d4db9599aedb8c9ac41c701a5ddd2e
Ivan Lott <ivanlott@hotmail.com>
2022-12-16 08:22:36 -0300

    corrigindo erros

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -14,7 +14,7 @@ export class Notification {
     private _id:string;
     private props: NotificationProps;
     
-    constructor(props: Replace<NotificationProps, {createdAt?: Date}>) {
+    constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
         this._id = randomUUID();
         this.props = {
             ...props,
